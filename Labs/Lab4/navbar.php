3df3c5e5ff1c5266d819a599e743d01067337b4f
U-JUSTINCPU\Justin <justinb8669@gmail.com>
2015-12-14 22:27:01 -0500

    Adding Links

--- Labs/Lab4/navbar.php
+++ Labs/Lab4/navbar.php
@@ -9,16 +9,18 @@
 
             <nav>
                 <ul>
-                    <li><a href="http://justinbennett.ca">Home</a></li>
+                    <li><a href="http://justinbennett.ca/">Home</a></li>
                     <li><a href="#">Projects</a>
 
                         <ul>
                             <li><a href="/CST8257/Labs/Lab3/">Loops</a></li>
                             <li><a href="/CST8257/Labs/Lab4/">Deposit Calculator</a></li>
                             <li><a href="/CST8257/Labs/Lab5/">Liquid Converter Tool</a></li>
+                            <li><a href="/CST8257/Labs/Lab6/Registration.php">Class Registration</a></li>
+                            <li><a href="/CST8257/Labs/Lab7/PictureGallery.php">Picture Gallery</a></li>
                         </ul>
                     </li>
-                    <li><a href="http://justinbennett.ca/about.php">About Me</a></li>
+                    <li><a href="http://www.justinbennett.ca/about.php">About Me</a></li>
                 </ul>
 
             </nav>
